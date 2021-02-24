9265c2b4bbf70c18d7f0219038131240a2431022
Hephka <franckthevenin95@gmail.com>
2021-02-24 16:42:39 +0100

    authentification

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -8,10 +8,12 @@ import {
 import Home from "../../pages/Home";
 import Profil from "../../pages/Profil";
 import Trending from "../../pages/Trending";
+import Navbar from "../Navbar";
 
 const index = () => {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/profil" component={Profil} />

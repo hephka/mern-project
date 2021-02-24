7cf15d36a285e1d83ca22ea4167f6e4dccab9aa9
Hephka <franckthevenin95@gmail.com>
2021-02-24 06:38:25 +0100

    start front - react & configs

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,44 +1,42 @@
 const UserModel = require("../models/user.model");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { signUpErrors, signInErrors } = require("../utils/errors.utils");
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
 const createToken = (id) => {
-    return jwt.sign({ id }, process.env.TOKEN_SECRET, {
-        expiresIn: maxAge
-    })
-}
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+    expiresIn: maxAge,
+  });
+};
 
 module.exports.signUp = async (req, res) => {
-    console.log(req.body);
-    const {pseudo, email, password} = req.body
+  const { pseudo, email, password } = req.body;
 
-    try {
-        const user = await UserModel.create({pseudo, email, password});
-        res.status(201).json({ user: user._id })
-    }
-    catch(err) {
-        const errors = signUpErrors(err);
-        res.status(200).send({ errors })
-    }
-}
+  try {
+    const user = await UserModel.create({ pseudo, email, password });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    const errors = signUpErrors(err);
+    res.status(200).send({ errors });
+  }
+};
 
 module.exports.signIn = async (req, res) => {
-    const { email, password } = req.body
+  const { email, password } = req.body;
 
-    try {
-        const user = await UserModel.login(email, password);
-        const token = createToken(user._id);
-        res.cookie('jwt', token, { httpOnly: true, maxAge });
-        res.status(200).json({ user: user._id });
-    } catch (err) {
-        const errors = signInErrors;
-        res.status(200).json({ err });
-    }
-}
+  try {
+    const user = await UserModel.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    const errors = signInErrors(err);
+    res.status(200).json({ errors });
+  }
+};
 
 module.exports.logout = (req, res) => {
-    res.cookie('jwt', '', { maxAge: 1 });
-    res.redirect('/');
-}
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
+};
